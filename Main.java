eda7ff32a8a685345ae5fb6b8db5c516d111a266
Zhina <zhina.gh2000@gmail.com>
2023-08-13 17:48:51 +0330

    added two files

---
+++ Main.java
@@ -0,0 +1,273 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello People. ihde isfhs msdfdjkw skjdhiuwhd salamxscd lijiijlf sjflsdfj slfjijls csljfdowejffsd sljfj sjfl si ljwpfdkapsdkpo  opoewjflds  pofspodjfdospow powoepolsklfdfp pppwioiur ijfdkjlsfk jfsiljflsdijfowo ojwojfjw oflwfjosj fjowjfls jlsfjidjfls fslifjiew fo oewiwjp joisoijfsdljjfilwfjsljfiejslkdj ljsflkdjfk j");
+System.out.println("How are you?");
+System.out.println("Are you ok?");
+System.out.print("I said one more time. ");
+System.out.println("for oldtime's sake.");
+/* another meaningless comment 
+with multiple lines */
+System.out.println(89.99);
+System.out.println(7+19);
+System.out.println(567*2537);
+// this comment means nothing
+String name = "Zhina";
+System.out.println(name);
+int Mynum = -78;
+Mynum = 45; //changed the number
+System.out.println(Mynum);
+final int Moo = 39;
+System.out.println(Moo);
+float Num = -23.55f;
+System.out.println(Num);
+char Ch = 'J';
+System.out.println(Ch);
+boolean Boo = false;
+System.out.println(Boo);
+String Name = "John";
+System.out.println("Fuck you " + Name);
+String first = "Zhina ";
+String second = "Gholami";
+String result = first + second;
+System.out.println(result);
+int x = 65;
+int y = 43;
+System.out.println(x+y);
+int h = 36 , a = 878 , z = 123;
+System.out.println(h+a+z);
+int l , d, v;
+l = d = v = 100;
+System.out.println(l+d+v);
+byte myByte = -100;
+System.out.println(myByte);
+short myShort = 22874;
+System.out.println(myShort);
+int myInt = 222222222;
+System.out.println(myInt);
+long myLong = 873745439878982326L;
+System.out.println(myLong);
+float mF = 4.58233f;
+System.out.println(mF);
+double mD = -2342.23492874030d;
+System.out.println(mD);
+boolean mB = true;
+System.out.println(mB);
+char mC = 'p';
+System.out.println(mC);
+float f1 = 12e5f;
+System.out.println(f1);
+double d1 = 3452E3d;
+System.out.println(d1);
+boolean isSheGood = true;
+boolean isShebad = false;
+System.out.println(isSheGood);
+System.out.println(isShebad);
+char ascii = 65; //this is an ascii number that represents a letter, in this cas A
+System.out.println(ascii);
+int a1 = 3;
+double b1 = a1;
+System.out.println(a1);
+System.out.println(b1);
+double c1 = 3.45d;
+int z1 = (int) c1;
+System.out.println(c1);
+System.out.println(z1);
+int o = 98, m = 99;
+System.out.println(o-m);
+int r = 473 , q = 8;
+System.out.println(r/q);
+System.out.println(r%q);
+int k = 78;
+++k;
+System.out.println(k);
+int n = 65;
+--n;
+System.out.println(n);
+int u = 45;
+u+=5;
+System.out.println(u);
+int b = 25;
+b-=7;
+System.out.println(b);
+int c = 786;
+c*=2;
+System.out.println(c);
+int g = 9823;
+g/=76;
+System.out.println(g);
+int f = 8743;
+f%=32;
+System.out.println(f);
+int s = 73 , p = 12;
+System.out.println(s>p);
+System.out.println(s<p);
+System.out.println(s==p);
+System.out.println(s!=p);
+System.out.println(s>=p);
+System.out.println(s<=p);
+System.out.println(s>20 && p<20);
+System.out.println(s==50 || p>5);
+System.out.println(!(s>20 && p<20));
+String txt = "ashjdsksdjhjhdsjdjkshdsjdhsjdsl oqwid o iwoqidodo oi dq";
+System.out.println("the length is:" + txt.length());
+String zin = "On The Nature of Daylight";
+System.out.println(zin);
+System.out.println(zin.toUpperCase());
+System.out.println(zin.toLowerCase());
+System.out.println(zin.indexOf("Nature"));
+String fName = "Lili ";
+String lName = "Andrews";
+System.out.println(fName + " " + lName);
+System.out.println(fName.concat(lName));
+String num = "346";
+String num1 = "7862";
+System.out.println(num + num1);
+int nN = 213;
+String num2 = num + nN;
+System.out.println(num2);
+String op = "We are \"The Ones\". ";
+String yu = "It\'s OK. ";
+String er = "This \\ is a backslash.";
+System.out.println(op + yu + er);
+String nk = "Zhina\nGholami";
+String ml = "GG\rGhola";
+String ay = "Zhina\tGholami";
+String xc = "Zhina\bGholami";
+System.out.println(nk);
+System.out.println(xc);
+System.out.println(ay);
+System.out.println(ml);
+String ff = "kscd\fds";
+System.out.println(ff);
+System.out.println(Math.max(683, 234));
+System.out.println(Math.min(32,12));
+System.out.println(Math.sqrt(124));
+System.out.println(Math.abs(-37));
+System.out.println(Math.random());
+int random = (int)(Math.random() *201);
+System.out.println(random);
+System.out.println(21<9);
+int kkw = 67;
+int wwk = 71;
+if (kkw < wwk) {
+System.out.println("this is true.");}
+else {
+System.out.println("that is false.");}
+int grade = 20;
+if (grade <=19) {
+System.out.println("very good");}
+else if (grade > 20) {
+System.out.println("unreal");}
+else {
+System.out.println("perfect");}
+int time = 22;
+String tResult = (time>=22) ? "good night" : "good evening";
+System.out.println(tResult);
+int g1 = 18;
+switch (g1) {
+case 20:
+System.out.println("perfect");
+break;
+case 19:
+System.out.println("very good");
+break;
+case 18:
+System.out.println("good");
+break;
+case 17:
+System.out.println("not bad");
+break;
+case 16:
+System.out.println("bad");
+break;
+default:
+System.out.println("awful");
+}
+int loop = 10;
+while (loop<=20) {
+System.out.println(loop);
+++loop;
+}
+int doloop = 1;
+do {
+System.out.println(doloop);
+++doloop;
+}
+while (doloop<7);
+for (int fors = 2; fors<6; fors=fors + 2) {
+System.out.println(fors);
+}
+for (int hg = 7; hg<10; hg++) {
+System.out.println("outer " + hg);
+for (int fg = 5; fg<8; fg++) {
+System.out.println("inner " + fg);
+}
+} 
+for (int kl = 1; kl<3; kl++) {
+System.out.println("yes");
+}
+for (int wc = 4; wc<10; wc++) {
+if (wc==8) {
+break;
+}
+System.out.println(wc);
+}
+for (int ns = 2; ns<8; ns++) {
+if (ns==5) {
+continue;
+}
+System.out.println(ns);
+}
+int sk = 3;
+while (sk<9) {
+sk++;
+System.out.println(sk);
+if (sk==7) {
+break;
+}
+} 
+int al = 0;
+while (al<6) {
+if (al==4) {
+++al;
+continue;
+}
+al++;
+System.out.println(al);
+}
+int dss = 1;
+while (dss<10) {
+if (dss==7) {
+dss++;
+continue;
+}
+System.out.println(dss);
+dss++;
+}
+String[] grs = {"ab" , "hj" , "wiu" , "weu"};
+System.out.println(grs[2]);
+int[] wqwe = {32, 432, 4243, 98, 320};
+wqwe[4] = 761;
+System.out.println(wqwe[4]);
+System.out.println(wqwe.length);
+String[] dnc = {"good" , "bad" , "great" , "perfect"};
+for (int lkv = 0; lkv<dnc.length; lkv++) {
+System.out.println(dnc[lkv]);
+}
+String[] lcn = {"alk" , "dajs" , "sadk" , "dajk"};
+for (String coq : lcn) {
+System.out.println(coq);
+}
+int[][] nunum = {{3,6,0,2} , {0,4,1,8}};
+System.out.println(nunum[0][1]);
+int[][] xal = {{3,6,0,2} , {0,4,1,8}};
+xal[0][1] = 10;
+System.out.println(xal[0][1]);
+int[][] xas = {{3,4,2,6} , {9,2,8,1}};
+for (int an = 0; an<xas.length; an++) {
+for (int na = 0; na<xas[an].length; na++) {
+System.out.println(xas[an][na]);
+}
+}
+ }
+}
